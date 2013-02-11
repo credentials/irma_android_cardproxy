b699e39caf0a26a29207cd4aad5d447103deb4ab
Maarten Everts <maarten.everts@tno.nl>
2013-02-11 17:49:30 +0100

    Cleanup of code, improve robustness.

--- src/org/irmacard/androidcardproxy/MainActivity.java
+++ src/org/irmacard/androidcardproxy/MainActivity.java
@@ -14,6 +14,7 @@ import net.sourceforge.scuba.smartcards.ResponseAPDU;
 import org.apache.http.entity.StringEntity;
 import org.json.JSONException;
 
+import service.IdemixService;
 import service.IdemixSmartcard;
 import service.ProtocolCommand;
 import service.ProtocolResponse;
@@ -54,192 +55,14 @@ public class MainActivity extends Activity {
 	private PendingIntent mPendingIntent;
 	private IntentFilter[] mFilters;
 	private String[][] mTechLists;
-	private byte[] lastTagUID;
 	private IsoDep lastTag = null;
 	private CommandSet lastCommandSet = null;
 	
 	private String TAG = "CardProxyMainActivity";
+	
+	private boolean tagReadyForProcessing = false;
 
-	private CardProxyConnection mCardProxyConnection;
-	private Handler mUpdateHandler;
 	
-	NsdHelper mNsdHelper;
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	    
-		
-        // NFC stuff
-        nfcA = NfcAdapter.getDefaultAdapter(getApplicationContext());
-        mPendingIntent = PendingIntent.getActivity(this, 0,
-                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
-
-        // Setup an intent filter for all TECH based dispatches
-        IntentFilter tech = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
-        mFilters = new IntentFilter[] { tech };
-
-        // Setup a tech list for all IsoDep cards
-        mTechLists = new String[][] { new String[] { IsoDep.class.getName() } };
-
-		
-		mUpdateHandler = new Handler() {
-	        @Override
-	        public void handleMessage(Message msg) {
-	            String chatLine = msg.getData().getString("msg");
-	            Log.i(TAG,chatLine);
-	        }
-	    };
-	    setState(STATE_IDLE);
-	    
-//		mCardProxyConnection = new CardProxyConnection(mUpdateHandler);
-//		mNsdHelper = new NsdHelper(this);
-//		mNsdHelper.initializeNsd();
-	}
-
-	@Override
-	protected void onPause() {
-		super.onPause();
-    	if (nfcA != null) {
-    		nfcA.disableForegroundDispatch(this);
-    	}
-	}
-	@Override
-	protected void onResume() {
-		super.onResume();
-        super.onResume();
-        if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(getIntent().getAction())) {
-            processIntent(getIntent());
-        }        
-        if (nfcA != null) {
-        	nfcA.enableForegroundDispatch(this, mPendingIntent, mFilters, mTechLists);
-        }
-	}
-	@Override
-	protected void onDestroy() {
-		// TODO Auto-generated method stub
-//		mNsdHelper.tearDown();
-//		mCardProxyConnection.tearDown();
-		super.onDestroy();
-	}
-	
-	public void onMainTouch(View v) {
-		if (activityState == STATE_IDLE) {
-			startQRScanner("Scan the QR image in the browser.");
-		}
-	}
-	
-    @Override
-    public void onNewIntent(Intent intent) {
-        Log.i(TAG, "Discovered tag with intent: " + intent);
-        setIntent(intent);
-    }
-    
-    public void processIntent(Intent intent) {
-        Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-    	IsoDep tag = IsoDep.get(tagFromIntent);
-    	if (tag != null) {
-    		Log.i(TAG,"Found IsoDep tag!");
-    		lastTagUID = tagFromIntent.getId();
-    		lastTag = tag;
-    		tryVerification();
-    	}    	
-    }
-	
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		IntentResult scanResult = IntentIntegrator
-				.parseActivityResult(requestCode, resultCode, data);
-		// Process the results from the QR-scanning activity
-		if (scanResult != null) {
-			String contents = scanResult.getContents();
-			if (contents != null) {
-				doInitialRequest(contents);
-			}
-		}
-	}
-	
-	public void doInitialRequest(String startURL) {
-		Log.i(TAG,startURL);
-//		AsyncHttpClient client = new AsyncHttpClient();
-//		client.get("http://192.168.1.104:8080/", new AsyncHttpResponseHandler() {
-//		    @Override
-//		    public void onSuccess(String response) {
-//		        Log.i(TAG,response);
-//		    }
-//		});
-
-		AsyncHttpClient client = new AsyncHttpClient();
-		RequestParams params = new RequestParams();
-//		params.put("Content-Type", "application/json");
-//		params.put("data", "{}");
-		try {
-			client.post(this, startURL, new StringEntity("") , "application/json",  new AsyncHttpResponseHandler() {
-			    @Override
-			    public void onSuccess(String response) {
-					Gson gson = new GsonBuilder().
-							setPrettyPrinting().
-							registerTypeAdapter(ProtocolCommand.class, new ProtocolCommandDeserializer()).
-							create();
-					lastCommandSet = gson.fromJson(response,CommandSet.class);
-					tryVerification();
-			    }
-			    @Override
-			    public void onFailure(Throwable arg0, String arg1) {
-			    	// TODO Auto-generated method stub
-			    	Log.e(TAG, "Failure: " + arg1);
-				}
-			});
-		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
-	
-	public void tryVerification() {
-		Log.i(TAG, "Trying to do verification");
-		if (lastTag != null && lastCommandSet != null) {
-			Log.i(TAG, "All set for verification!");
-			setState(STATE_CHECKING);
-			new ProcessCommandSet().execute(new SmartcardProxyInput(lastCommandSet, lastTag));
-			lastTag = null;
-		} else if (lastTag == null) {
-			setState(STATE_WAITING);
-		}
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
-	
-	public void startQRScanner(String message) {
-		IntentIntegrator integrator = new IntentIntegrator(this);
-    	integrator.initiateScan(IntentIntegrator.QR_CODE_TYPES, message);
-	}
-	
-	public void broadcast() {
-		if (mCardProxyConnection.getLocalPort() > -1) {
-			mNsdHelper.registerService(mCardProxyConnection.getLocalPort());
-		} else {
-			Log.d(TAG, "ServerSocket isn't bound.");
-		}
-	}
-	
-	private class SmartcardProxyInput {
-		public CommandSet cs;
-		public IsoDep tag;
-		SmartcardProxyInput(CommandSet cs, IsoDep tag) {
-			this.cs = cs;
-			this.tag = tag;
-		}
-	}
-
 	private int activityState = STATE_WAITING;
 	
 	private static final int STATE_WAITING = 0;
@@ -261,7 +84,7 @@ public class MainActivity extends Activity {
     	switch (activityState) {
     	case STATE_WAITING:
     		imageResource = R.drawable.irma_icon_place_card_520px;
-    		statusTextResource = R.string.status_waiting;
+    		statusTextResource = R.string.status_waiting;    		
     		break;
 		case STATE_CHECKING:
 			imageResource = R.drawable.irma_icon_card_found_520px;
@@ -282,6 +105,7 @@ public class MainActivity extends Activity {
 		case STATE_IDLE:
 			imageResource = R.drawable.irma_icon_place_card_520px;
 			statusTextResource = R.string.status_idle;
+			lastTag = null;
 		default:
 			break;
 		}
@@ -310,10 +134,148 @@ public class MainActivity extends Activity {
 		((ImageView)findViewById(R.id.statusimage)).setImageResource(imageResource);
 	}
 	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+        // NFC stuff
+        nfcA = NfcAdapter.getDefaultAdapter(getApplicationContext());
+        mPendingIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+        // Setup an intent filter for all TECH based dispatches
+        IntentFilter tech = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+        mFilters = new IntentFilter[] { tech };
+
+        // Setup a tech list for all IsoDep cards
+        mTechLists = new String[][] { new String[] { IsoDep.class.getName() } };
+		
+	    setState(STATE_IDLE);
+
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+    	if (nfcA != null) {
+    		nfcA.disableForegroundDispatch(this);
+    	}
+	}
+	@Override
+	protected void onResume() {
+        super.onResume();
+        if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(getIntent().getAction())) {
+            processIntent(getIntent());
+        }        
+        if (nfcA != null) {
+        	nfcA.enableForegroundDispatch(this, mPendingIntent, mFilters, mTechLists);
+        }
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+	
+	public void onMainTouch(View v) {
+		if (activityState == STATE_IDLE) {
+			lastTag = null;
+			startQRScanner("Scan the QR image in the browser.");
+		}
+	}
+	
+    @Override
+    public void onNewIntent(Intent intent) {
+        tagReadyForProcessing = true;
+        setIntent(intent);
+    }
+    
+    public void processIntent(Intent intent) {
+        Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+    	IsoDep tag = IsoDep.get(tagFromIntent);
+    	if (tag != null) {
+    		lastTag = tag;
+    		tryVerification();
+    	}    	
+    }
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		IntentResult scanResult = IntentIntegrator
+				.parseActivityResult(requestCode, resultCode, data);
+
+		// Process the results from the QR-scanning activity
+		if (scanResult != null) {
+			String contents = scanResult.getContents();
+			if (contents != null) {
+				doInitialRequest(contents);
+			}
+		}
+	}
+	
+	public void doInitialRequest(String startURL) {
+
+		AsyncHttpClient client = new AsyncHttpClient();
+		try {
+			client.post(this, startURL, new StringEntity("") , "application/json",  new AsyncHttpResponseHandler() {
+			    @Override
+			    public void onSuccess(String response) {
+					Gson gson = new GsonBuilder().
+							setPrettyPrinting().
+							registerTypeAdapter(ProtocolCommand.class, new ProtocolCommandDeserializer()).
+							create();
+					lastCommandSet = gson.fromJson(response,CommandSet.class);
+					setState(STATE_WAITING);
+					tryVerification();
+			    }
+			    @Override
+			    public void onFailure(Throwable arg0, String arg1) {
+			    	// TODO: feedback to user that command failed!
+			    	Log.e(TAG, "Failure: " + arg1);
+				}
+			});
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	public void tryVerification() {
+		if (activityState == STATE_WAITING && lastTag != null && tagReadyForProcessing) {
+			setState(STATE_CHECKING);
+			tagReadyForProcessing = false;
+			new ProcessCommandSet().execute(new SmartcardProxyInput(lastCommandSet, lastTag));
+		}
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+	
+	public void startQRScanner(String message) {
+		IntentIntegrator integrator = new IntentIntegrator(this);
+    	integrator.initiateScan(IntentIntegrator.QR_CODE_TYPES, message);
+	}
+	
+	private class SmartcardProxyInput {
+		public CommandSet cs;
+		public IsoDep tag;
+		SmartcardProxyInput(CommandSet cs, IsoDep tag) {
+			this.cs = cs;
+			this.tag = tag;
+		}
+	}
+	
 	private class SmartcardProxyOutput {
 		public CommandSet cs;
 		public ProtocolResponses responses;
 	}
+	
 	private class ProcessCommandSet extends AsyncTask<SmartcardProxyInput, Void, SmartcardProxyOutput> {
 
 		@Override
@@ -324,19 +286,20 @@ public class MainActivity extends Activity {
 			// Make sure time-out is long enough (10 seconds)
 			tag.setTimeout(10000);
 			
-			CardService cardser = new IsoDepCardService(tag);
+			IdemixService is = new IdemixService(new IsoDepCardService(tag));
 			
 			SmartcardProxyOutput smartcardOutput = new SmartcardProxyOutput();
 			smartcardOutput.cs = cs;
 			smartcardOutput.responses = new ProtocolResponses();
 			try {
-				cardser.open();
-				cardser.transmit(IdemixSmartcard.selectAppletCommand.getAPDU());
+				is.open();
+				is.selectApplet();
+				
 				for (ProtocolCommand c : cs.commands) {
 					smartcardOutput.responses.put(c.getKey(), 
-							new ProtocolResponse(c.getKey(), cardser.transmit(c.getAPDU())));
+							new ProtocolResponse(c.getKey(), is.transmit(c.getAPDU())));
 				}
-				cardser.close();
+				is.close(); 
 			} catch (CardServiceException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -356,9 +319,7 @@ public class MainActivity extends Activity {
 					create();
 			
 			String httpresult = gson.toJson(result.responses);
-			Log.i(TAG,httpresult);
 			AsyncHttpClient client = new AsyncHttpClient();
-			Log.i(TAG,"responseurl: " + result.cs.responseurl);
 			try {
 				client.post(MainActivity.this, result.cs.responseurl, new StringEntity(httpresult) , "application/json",  new JsonHttpResponseHandler() { 
 					public void onSuccess(org.json.JSONObject jsonobj) {
@@ -370,17 +331,15 @@ public class MainActivity extends Activity {
 								setState(STATE_RESULT_MISSING);
 							}
 							lastCommandSet = null;
+							lastTag = null;
 						} catch (JSONException e) {
-							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
 					};
 				});
 			} catch (UnsupportedEncodingException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-
 		}
 	}
 }
